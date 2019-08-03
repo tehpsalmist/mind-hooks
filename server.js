16949296635896ebfd279f1bc5dc284480d96934
Glitch (the-mind-static-hooks) <none>
2019-08-03 07:59:14 +0000

    🚜🖇 Updated with Glitch

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.post('/game-updated', require('./game-updated'))
 app.post('/player-updated', require('./player-updated'))
 app.get('/new-user/:userId', require('./new-user'))
 app.post('/message-sent', require('./message-sent'))
+app.get('/clear-db', require('./clear-db'))
 
 const listener = app.listen(process.env.PORT, () => {
   console.log('The Mind is live on port ' + listener.address().port)
